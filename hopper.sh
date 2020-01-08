2274873e321cf1907e93c79f5f86a00e374814f2
Madnyax <koji.learning@gmail.com>
2020-01-08 13:44:07 +0900

    add hopper.sh

--- hopper.sh
+++ hopper.sh
@@ -2,9 +2,9 @@
 
 while true  
 do  
-    for channel in {1..14}
+    for channel in {1..13}
     do
-        iwconfig wlan1 channel $channel
+        sudo iwconfig wlan1 channel $channel
         sleep 0.1s
     done
 done  
