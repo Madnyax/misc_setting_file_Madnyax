185af9f892bda2388497c296c72a0d1280e44b89
Madnyax <koji.learning@gmail.com>
2020-07-02 17:32:46 +0900

    left only vimrc file

--- c_mode_change.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo ifconfig wlan1 down
-sudo iwconfig wlan1 mode Monitor
-sudo ifconfig wlan1 up
-
--- mode_change.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo ifdown wlan1
-sudo iwconfig wlan1 mode Monitor
-sudo ifup wlan1
-
